8597521b03fe4c3cd7c399e16222a18f773e672f
shiro <ryouta@shiraishiryoutanoMacBook-Air.local>
2023-09-22 09:42:43 +0900

    サンプル

---
+++ app/main.js
@@ -0,0 +1,11 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
